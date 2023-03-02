a77fc165ee91ede9303990ef838462c0341001b4
PeterSlovak <xslovakp3@stuba.sk>
2023-03-02 23:18:04 +0100

    another window shit

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ from PySide6.QtCore import QFile, QIODevice
 import os
 import glob
 from PySide6.QtGui import QPixmap, QImage, QResizeEvent
+import matplotlib 
+matplotlib.use('tkagg')
 
 def loading_data(name_file, fuzzy_flag):
     places, transitions, arcs, role = read_xml(name_file, fuzzy_flag)
@@ -60,6 +62,10 @@ class AnotherWindow(QWidget):
         self.window.setWindowTitle("Initial marking")
         self.ui.close()
         self.window.show()
+        self.logical_flag = False
+        self.fuzzy_flag = False
+        self.fuzzy_weights_flag = False
+        self.fuzzy_weights_tresholds_flag = False
         self.window.enter.clicked.connect(MainAplication.run_final)
        
     def set_marking_initial(self, net, root, fuzzy, file_name, tree, weights, tresholds):
@@ -150,6 +156,7 @@ class AnotherWindow(QWidget):
                     l += 1
         if fuzzy:
             if tresholds:
+                self.fuzzy_weights_tresholds_flag = True
                 if len(net.tresholds) == 0:
                     net.tresholds = [0 for _ in range(len(dict_transitions))]
                     # create new tag to xml to each transition
@@ -166,6 +173,7 @@ class AnotherWindow(QWidget):
                         # add value to new tag
                         new_tag.text = str(net.tresholds[i])
             if weights:
+                self.fuzzy_weights_flag = True
                 l = 0
                 if len(net.multiplicities) == 0:
                     net.multiplicities = [1 for _ in range(len(dict_weights))]
@@ -180,6 +188,10 @@ class AnotherWindow(QWidget):
                             if value.tag == 'multiplicity':
                                 value.text = str(net.multiplicities[l])
                                 l += 1
+            else:
+                self.fuzzy_flag = True
+        else:
+            self.logical_flag = True
 
         tree.write(file_name + "_marking.xml",
                 encoding="UTF-8", xml_declaration=True)
@@ -357,20 +369,28 @@ class MainAplication(QtWidgets.QMainWindow):
         self.actual_marking_dict = {}
         self.TR = []
         self.k = 0
-        self.logical_flag = 0
-        self.fuzzy_flag = 0
-        self.fuzzy_weights_flag = 0
-        self.fuzzy_weights_tresholds_flag = 0
+        self.logical_flag = False
+        self.fuzzy_flag = False
+        self.fuzzy_weights_flag = False
+        self.fuzzy_weights_tresholds_flag = False
         self.tree = None
+        self.anotherWindow = AnotherWindow()
+        #self.tree = None
 
         if self.image_number == 1:
             self.main_layout.prevButton.setEnabled(False)
 
     # resize event
+    
     def resizeEvent(self, event):
-        print("resize")
+        # setImage
+        if len(self.image_dict)>0:
+            prem = QImage(self.image_dict[self.image_number])
+            pixmap = QPixmap.fromImage(prem)
+            self.main_layout.photo.setPixmap(pixmap.scaled(self.main_layout.photo.size(), QtCore.Qt.KeepAspectRatio, QtCore.Qt.SmoothTransformation))
+            self.main_layout.photo.setAlignment(QtCore.Qt.AlignCenter)
         event.accept()
-
+    
     def open_dialog(self):
         fname = QFileDialog.getOpenFileName(
             self, 'Open file', 'c:\\', "XML files (*.xml)")
@@ -397,53 +417,55 @@ class MainAplication(QtWidgets.QMainWindow):
                 fuzzy = 0
             net = loading_data(self.file_name, fuzzy)
             if self.main_layout.comboBox.currentText() == "Logická Petriho sieť":
-                self.logical_flag = 1
+                self.logical_flag = True
                 self.anotherWindow = AnotherWindow()
-                self.anotherWindow.set_marking_initial(net, root, fuzzy, self.file_name.split('.')[0] + "_initial",self.tree,0,0)
+                self.anotherWindow.set_marking_initial(net, root, fuzzy, self.file_name.split('.')[0] + "_initial", self.tree,0,0)
                 
                 #set_initial_marking(net, root, fuzzy, self.file_name.split('.')[0] + "_initial",tree,0,0)
        
             elif self.main_layout.comboBox.currentText() == "Fuzzy Petriho sieť":
-                self.fuzzy_flag = 1
+                self.fuzzy_flag = True
                 self.anotherWindow = AnotherWindow()
                 self.anotherWindow.set_marking_initial(net, root, fuzzy, self.file_name.split('.')[0] + "_initial",self.tree,0,0)
                 
                 #set_initial_marking(net, root, fuzzy, self.file_name.split('.')[0] + "_initial",tree,0,0)
 
             elif self.main_layout.comboBox.currentText() == "Fuzzy Petriho sieť s váhami pravidiel":
-                self.fuzzy_weights_flag = 1
+                self.fuzzy_weights_flag = True
                 self.anotherWindow = AnotherWindow()
                 self.anotherWindow.set_marking_initial(net, root, fuzzy, self.file_name.split('.')[0] + "_initial",self.tree,1,0)
                 
                 #set_initial_marking(net, root, fuzzy, self.file_name.split('.')[0] + "_initial",tree,1,0)
      
             elif self.main_layout.comboBox.currentText() == "Fuzzy Petriho sieť s váhami a prahmi pravidiel":
-                self.fuzzy_weights_tresholds_flag = 1
-                self.anotherWindow = AnotherWindow()
+            
                 self.anotherWindow.set_marking_initial(net, root, fuzzy, self.file_name.split('.')[0] + "_initial",self.tree,1,1)
                 #set_initial_marking(net, root, fuzzy, self.file_name.split('.')[0] + "_initial",tree,1,1)
                 self.TR = net.tresholds
+                #self.fuzzy_weights_tresholds_flag = True
+                #net = loading_data(self.file_name.split('.')[0] + "_initial_marking.xml", 1)
+                #self.run_fuzzy_with_weights_and_thresholds(net, tree, self.file_path)
 
         else:
-            self.tree = None
+            #self.tree = None
             dialog = QMessageBox(text="Nevybrali ste žiadny súbor")
             dialog.setWindowTitle("Message Dialog")
             ret = dialog.exec()   # Stores the return value for the button pressed
-
+    
     def run_final(self):
-        if self.logical_flag == 1:
+        if self.anotherWindow.logical_flag:
             net = loading_data(self.file_name.split('.')[0] + "_initial_marking.xml", 0)
             self.run_logical(net, self.tree, self.file_path)
-        elif self.fuzzy_flag == 1:
+        elif self.anotherWindow.fuzzy_flag:
             net = loading_data(self.file_name.split('.')[0] + "_initial_marking.xml", 1)
             self.run_fuzzy(net, self.tree, self.file_path)
-        elif self.fuzzy_weights_flag == 1:
+        elif self.anotherWindow.fuzzy_weights_flag:
             net = loading_data(self.file_name.split('.')[0] + "_initial_marking.xml", 1) 
             self.run_fuzzy_with_weights(net, self.tree, self.file_path)
-        elif self.fuzzy_weights_tresholds_flag == 1:
+        elif self.anotherWindow.fuzzy_weights_tresholds_flag:
             net = loading_data(self.file_name.split('.')[0] + "_initial_marking.xml", 1)
             self.run_fuzzy_with_weights_and_thresholds(net, self.tree, self.file_path)
-
+    
 
     def prev(self):
         if self.image_number > 1:
